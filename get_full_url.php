ad0315d3efd6bca041e0c14fef7d70ec9ba4e8cd
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-07 11:41:05 +0800

    Update get_full_url.php

--- get_full_url.php
+++ get_full_url.php
@@ -1,4 +1,4 @@
 <meta http-equiv="refresh" content="0; url=/uploads.list" />
 <?php
-  exec("ls uploads/ > uploads.list");
+  exec('ls uploads/ | sed "s|^|https://'.$_SERVER['SERVER_NAME'].'/uploads/|g" > uploads.list');
 ?>
