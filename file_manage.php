0dc87bdff5d1e62977dac28e893265c2e99559c3
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-07 10:45:17 +0800

    Create file_manage.php

---
+++ file_manage.php
@@ -0,0 +1,16 @@
+<?php
+  echo_folder("uploads");
+
+  function echo_folder($folder){ 
+    $output_file = shell_exec("ls ".$folder.' | tee '.substr($folder, 0, -1).'.list');
+    $output_file = explode("\n", $output_file);
+    array_pop($output_file);
+    foreach($output_file as $file){
+      $file_path = '/'.$folder.$file;
+      echo '<a href="'.$file_path.'" target="_blank" download>'.$file.'</a>';
+      echo '&#160;';
+      echo '<a href="'.$file_path.'" target="_blank">preview</a>';
+      echo '<br>';
+    }
+    
+?>
