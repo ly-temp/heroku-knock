9a0b758f74aeb0f451729bb1cd1ea476721fc889
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-07 10:44:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  <input name="upload[]" type="file" multiple />
+  <input type="submit" value="Send files" />
+</form>
+<a href="file_manage.php">download / manage</a>
