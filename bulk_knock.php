22457dd67c4ea9c84dcd2c339299746fdf9805c0
laiyuantemp <100673133+laiyuantemp@users.noreply.github.com>
2022-08-07 11:20:01 +0800

    Update bulk_knock.php

--- bulk_knock.php
+++ bulk_knock.php
@@ -1,6 +1,6 @@
 <meta http-equiv="refresh" content="0; url=/file_manage.php" />
 <?php
   chdir("uploads");
-  exec("chmod +x ~/bash/*");
-  exec("~/bash/bulk_knock.sh > ~/log.txt 2>&1 &");
+  exec("chmod +x ../bash/*");
+  exec("../bash/bulk_knock.sh > ~/log.txt 2>&1 &");
 ?>
