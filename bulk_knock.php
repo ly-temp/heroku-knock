12113f636d64f50dd132b50301e671685ed304c0
ly-temp <100673133+ly-temp@users.noreply.github.com>
2022-09-10 17:46:29 +0800

    Update bulk_knock.php

--- bulk_knock.php
+++ bulk_knock.php
@@ -1,7 +1,6 @@
 <meta http-equiv="refresh" content="0; url=/file_manage.php" />
 <?php
   exec("chmod +x bash/*");
-  exec("chmod +x no-backup/*");
   chdir("uploads");
   exec("../bash/buik_knock.sh > ../log.txt 2>&1 &");
 ?>
